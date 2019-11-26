ae97cfce2c5a46830c3370dce46a18cee2daacdd
pawurb <p.urbanek89@gmail.com>
2019-11-26 23:41:33 +0100

    initial green

--- lib/abstract_base/main.rb
+++ lib/abstract_base/main.rb
@@ -1,8 +1,21 @@
+require 'byebug'
+
+AbstractInitializationError = Class.new(StandardError)
+
 module AbstractBase
   def abstract_methods(*method_names)
     method_names.each do |name|
       define_method name do
         raise NotImplementedError
+      end
+    end
+  end
+
+  def abstract_class arg
+    abstract_class_name = self.to_s
+    define_method :initialize do |*parameters|
+      unless self.class.to_s != abstract_class_name
+        raise AbstractInitializationError
       end
     end
   end
--- test/test_abstract_base.rb
+++ test/test_abstract_base.rb
@@ -1,22 +1,33 @@
 require "test/unit"
 require_relative '../lib/abstract_base/main'
 
-class TestClass
+class BaseTestClass
   extend AbstractBase
 
   abstract_methods :call, :run
+  abstract_class true
 
   def execute
     true
   end
 end
 
-def test_object
-  TestClass.new
+class ChildTestClass < BaseTestClass
+  def initialize
+    super
+  end
+end
+
+def test_object_base
+  BaseTestClass.new
+end
+
+def test_object_child
+  ChildTestClass.new
 end
 
 class AbstractBaseTest < Test::Unit::TestCase
-  def test_abstract
+  def stest_abstract_methods
     assert_raise NotImplementedError do
       test_object.call
     end
@@ -27,6 +38,16 @@ class AbstractBaseTest < Test::Unit::TestCase
 
     assert_nothing_raised do
       test_object.execute
+    end
+  end
+
+  def test_abstract_initializer
+    assert_raise AbstractInitializationError do
+      test_object_base
+    end
+
+    assert_nothing_raised do
+      test_object_child
     end
   end
 end
